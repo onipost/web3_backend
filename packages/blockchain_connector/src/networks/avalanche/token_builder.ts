dd87b0029bc51b419bc3e0f3793098d49c482034
Roman Stepanov <roman.a.stepanov@gmail.com>
2023-05-01 03:53:09 +0700

    Add default abi

--- packages/blockchain_connector/src/networks/avalanche/token_builder.ts
+++ packages/blockchain_connector/src/networks/avalanche/token_builder.ts
@@ -1,18 +1,17 @@
 import { Network } from '@web3/common'
 import { Token } from '../../core/entity/Token'
-import usdcAbi from './abi/usdc_abi'
-import usdtAbi from './abi/usdt_abi'
+import defaultAbi from '../../core/abi/default_abi'
 
 export function avalancheAvax(): Token {
   return createToken('AVAX', '')
 }
 
 export function avalancheUsdc(): Token {
-  return createToken('USDC', '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', usdcAbi)
+  return createToken('USDC', '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', defaultAbi)
 }
 
 export function avalancheUsdt(): Token {
-  return createToken('USDT', '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7', usdtAbi)
+  return createToken('USDT', '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7', defaultAbi)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
--- packages/blockchain_connector/src/networks/polygon/token_builder.ts
+++ packages/blockchain_connector/src/networks/polygon/token_builder.ts
@@ -1,18 +1,17 @@
 import { Network } from '@web3/common'
 import { Token } from '../../core/entity/Token'
-import usdcAbi from './abi/usdc_abi'
-import usdtAbi from './abi/usdt_abi'
+import defaultAbi from '../../core/abi/default_abi'
 
 export function polygonMatic(): Token {
   return createToken('MATIC', '')
 }
 
 export function polygonUsdc(): Token {
-  return createToken('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', usdcAbi)
+  return createToken('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', defaultAbi)
 }
 
 export function polygonUsdt(): Token {
-  return createToken('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', usdtAbi)
+  return createToken('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', defaultAbi)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
