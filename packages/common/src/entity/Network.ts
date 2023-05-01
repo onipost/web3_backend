5d9985751b2c41ba3958a9ca538bc71419258160
Roman Stepanov <roman.a.stepanov@gmail.com>
2023-05-01 20:29:00 +0700

    Remove unsupported

--- packages/common/src/entity/Network.ts
+++ packages/common/src/entity/Network.ts
@@ -2,11 +2,8 @@ export enum Network {
   Etherium = 'Etherium',
   Arbitrum = 'Arbitrum',
   Optimism = 'Optimism',
-  ZkSync = 'ZkSync',
-  StarkNet = 'StarkNet',
   BSC = 'BSC',
   Avalanche = 'Avalanche',
-  Aptos = 'Aptos',
   Fantom = 'Fantom',
   Polygon = 'Polygon',
 }
